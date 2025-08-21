159805c29ab1d42d30efffa432412d60d8c3434f
Vincenzo Bonatti <vincenzobonati@outlook.com>
2025-08-21 20:37:24 -0300

    scroll do feed funcionando.

--- src/feed/App.js
+++ src/feed/App.js
@@ -1,49 +1,44 @@
 'use client';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 import { access } from '../Access';
 
+const token = await access();
 
-async function feed(token, obj){
-  let responses = []
-    for(let i = 0; i < 8; i++){
-        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
-        let randomSearch = '';
-        let getRandomOffset = Math.floor(Math.random() * 999);
+async function feed(token, obj) {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+  let randomSearch = '';
+  let getRandomOffset = Math.floor(Math.random() * 999);
 
 
-        switch (Math.round(Math.random() * 2)) {
-            case 0:
-                randomSearch = randomCharacter + '%25';
-            break;
-            case 1:
-                randomSearch = '%25' + randomCharacter + '%25';
-            break;
-            case 2:
-                randomSearch = '%25' + randomCharacter;
-            break;
-        }
+  switch (Math.round(Math.random() * 2)) {
+    case 0:
+      randomSearch = randomCharacter + '%25';
+      break;
+    case 1:
+      randomSearch = '%25' + randomCharacter + '%25';
+      break;
+    case 2:
+      randomSearch = '%25' + randomCharacter;
+      break;
+  }
 
-        let url = `https://api.spotify.com/v1/search?query=${randomSearch}&offset=${getRandomOffset}&limit=1&type=${obj}&market=NL`;
+  let url = `https://api.spotify.com/v1/search?query=${randomSearch}&offset=${getRandomOffset}&limit=1&type=${obj}&market=NL`;
 
-        const result = fetch(url, {
-            headers: {'Authorization': "Bearer " + token}
-        }).then(data => {
-            return data.json()
-        }).then(response =>{
-          if(obj === "artist"){
-            return response.artists.items[0];
-          } else if(obj === "album"){
-            return response.albums.items[0];  
-          }
-        })
-        responses.push(await result);
-        
-      }
-      console.log(responses)
-    return responses;
+  const result = fetch(url, {
+    headers: { 'Authorization': "Bearer " + token }
+  }).then(data => {
+    return data.json()
+  }).then(response => {
+    if (obj === "artist") {
+      return response.artists.items[0];
+    } else if (obj === "album") {
+      return response.albums.items[0];
+    }
+  })
+  return result;
 }
 
 
@@ -52,28 +47,60 @@ function FeedPagina() {
   const [feedAlbum, setFeedAlbum] = useState([]);
   const [feedArtist, setFeedArtist] = useState([]);
 
-  useEffect(() => {
-    async function fetchToken() {
-      let token = await access();
-      const artists = await feed(token, "artist");
-      const albums = await feed(token, "album");
+  const handleScroll = async () => {
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight
 
-      setFeedArtist(artists);
-      setFeedAlbum(albums);
+    if (bottom) {
+      const albums = await feed(token, "album");
+      setFeedAlbum(oldArray => [...oldArray, albums]);
     }
+  };
+
+  async function fetchToken() {
+    const artists = await feed(token, "artist");
+    const albums = await feed(token, "album");
+    if (!artists || !albums) {
+      console.error("Failed to fetch artists or albums");
+      return;
+    }
+    console.log(albums);
+    setFeedArtist(oldArray => [...oldArray, artists]);
+    setFeedAlbum(oldArray => [...oldArray, albums]);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, {
+      passive: true
+    })
     fetchToken();
   }, []);
 
   return (
     <main>
-      {feedAlbum.length > 0 ? (
+      {feedAlbum ? (
         <pre>
-          {feedAlbum.map((item, index) => (
-            <div key={index}>
-              <h2>{item.name}</h2>
-              <p>{item.artists.map(artist => artist.name).join(', ')}</p>
-            </div>
-          ))}
+          <div className="feed_container">
+            {feedAlbum.map(album => (
+              <div className="post" key={album.id}>
+                <Link to={`/album/${album.id}`}>
+                  <img src={album.images[0]?.url} alt={album.name} />
+                  <div className="descricao">
+                    <div className="info">
+                      <h1>{album.name}</h1>
+                      <div className="infos"><p>Artistas: </p>{album.artists.map(artist => artist.name).join(", ")}</div>
+                      <div className="infos"><p>Musicas: </p>{album.total_tracks}</div>
+                      <div className="infos"><p>Tipo: </p>{album.album_type}</div>
+                      <div className="infos"><p>Lançamento(A/M/D): </p>{album.release_date.replace(/-/g, "/")}</div>
+                    </div>
+                  </div>
+                  <div className="acoes">
+                    <button>Curtir</button>
+                    <button>Compartilhar</button>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
         </pre>
       ) : (
         <p>Loading...</p>
